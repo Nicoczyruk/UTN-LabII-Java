b7d667cf9c851cdc53bacce764e98662b68d3ba8
Nicoczyruk <nicoczyruk2@gmail.com>
2023-08-11 11:29:57 -0300

    Primer commit del proyecto

---
+++ src/Holamundo.java
@@ -0,0 +1,11 @@
+
+public class Holamundo {
+    public static void main(String args[]) {
+
+        System.out.println("Hola mundo");
+        System.out.println("Primer trabajo lab II");
+
+    }
+}
+
+
